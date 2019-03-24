4c366f807521bceec53d0e5df6a4c5cb95e5e2f2
isortegah <isortegah@gmail.com>
2019-03-24 09:13:13 -0600

    Update pom jUnit4
    Revisar ejecución de la clase JunitParallel con maven

--- jUnit4/src/test/java/methodMain/JunitParallel.java
+++ jUnit4/src/test/java/methodMain/JunitParallel.java
@@ -1,12 +1,36 @@
 package methodMain;
 
-import org.junit.Test;
+import org.junit.*;
 
 import cucumber.api.cli.Main;
 
 public class JunitParallel {
+
+    @BeforeClass
+    public static void beforeClassFunction(){
+        System.out.println("Before Class");
+    }
+
+    @Before
+    public void beforeFunction(){
+        System.out.println("Before Function");
+    }
+
+    @After
+    public void afterFunction(){
+        System.out.println("After Function");
+    }
+
+    @AfterClass
+    public static void afterClassFunction(){
+        System.out.println("After Class");
+    }
+
+    /**
+     * ToDo: Ejecución vía Maven
+     */
     @Test
     public void test() {
-        Main.main(new String[]{"--threads", "4","-p","pretty","-g", "stepdef", "src/test/resources/features/"});
+        Main.main(new String[]{"--threads", "2","-p","pretty","-g", "stepdef", "src/test/resources/features/"});
     }
 }
